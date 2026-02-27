f28e87e787a093349b6f2b79b22434e00e859f45
Gokul Malik <gokulmlk21@gmail.com>
2026-02-27 23:47:46 +0530

    Express App is created for checking

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Project Initialization")
+const express = require("express")
+const app = express()
+
+const PORT = 3000
+
+app.get("/", (req,res)=> {
+    res.send("Hello from the server")
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running in the ${PORT}`)
+})
