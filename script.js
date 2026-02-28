f3935dbe3df0cdc88c252ac3e486b6537a68b413
Gokul Malik <gokulmlk21@gmail.com>
2026-02-28 21:59:02 +0530

    Product model created

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 const express = require("express")
-const app = express()
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
 
-const PORT = 3000
+
+
+
+dotenv.config();// Load environment variables
+
+
+connectDB();// Connect to MongoDB
+
+const app = express()// Initialize Express app
+
+
+app.use(cors()); // Enable CORS
+app.use(express.json()); // Parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+
+const PORT = process.env.PORT || 3001
 
 app.get("/", (req,res)=> {
     res.send("Hello from the server")
