3c88e4c0bece000404e0f06874b07e8f43baa4a9
Gokul Malik <gokulmlk21@gmail.com>
2026-02-28 23:31:20 +0530

    authController created for control authrouts

--- script.js
+++ script.js
@@ -2,24 +2,29 @@ const express = require("express")
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
-
-
-
+const errorHandler = require('./middleware/errorHandler');
 
 dotenv.config();// Load environment variables
-
 
 connectDB();// Connect to MongoDB
 
 const app = express()// Initialize Express app
 
-
+// Middleware
 app.use(cors()); // Enable CORS
 app.use(express.json()); // Parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 
+// Routes
+
+// Welcome route
+
+
+app.use(errorHandler);// Error handler middleware 
+
 const PORT = process.env.PORT || 3001
 
+//Start Server 
 app.get("/", (req,res)=> {
     res.send("Hello from the server")
 })
